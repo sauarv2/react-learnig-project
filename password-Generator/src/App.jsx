813d277a3002565e604ab45647a631fa73f8b3eb
saurav saha <sauravsaha024@gmail.com>
2025-04-10 21:55:05 +0530

    jk

--- password-Generator/src/App.jsx
+++ password-Generator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from "react";
+import { useState, useCallback, useEffect, useRef } from "react";
 
 import "./App.css";
 
@@ -11,30 +11,35 @@ function App() {
 
   const [password, setPassword] = useState("");
 
+  const passwordref = useRef(null);
+
   const passwordGenerator = useCallback(() => {
     let pass = "";
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklnopqrstuvwxyz";
     let num = "0123456789";
     let char = "~!@#$%^&*()_+";
-
     if (number) str += num;
     if (charallowed) str += char;
 
     for (let i = 0; i <= length; i++) {
       const genP = Math.floor(Math.random() * str.length + 1);
       pass += str.charAt(genP);
-
-      // console.log(pass);
     }
     setPassword(pass);
-    console.log(setPassword);
+    // console.log(setPassword);
   }, [length, number, charallowed, setPassword]);
 
   useEffect(() => {
     passwordGenerator();
   }, [length, number, charallowed, passwordGenerator]);
-  console.log(passwordGenerator);
+  // console.log(passwordGenerator);
 
+  // **************************************************
+  const copyPassword = useCallback(() => {
+    passwordref.current?.select();
+    passwordref.current?.setSelectionRange(0, 15);
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
   return (
     <>
       <div className="container">
@@ -88,9 +93,12 @@ function App() {
           className="output"
           placeholder="Your password will appear here"
           readOnly
+          ref={passwordref}
         />
 
-        {/* <button className="generate-btn">Generate Password</button> */}
+        <button onClick={copyPassword} className="generate-btn">
+          Copy
+        </button>
       </div>
     </>
   );
